96bb34c71372d386f8069b323763db6a72fd61a2
OniQ <oniq.eg@gmail.com>
2014-03-13 17:09:58 +0200

    add Graph class with constructor and comments

--- src/graph.py
+++ src/graph.py
@@ -1,6 +1,26 @@
 class Vertex:
     def __init__(self, name):
-        self.name = name
-        self.index = None
-        self.lowlink = None
-        self.value = None
+        self.name = name    #vertex name refers an expression variable
+        self.index = None   #index used by Tarjan's algorithm
+        self.lowlink = None #lowlink used by Tarjan's algorithm
+        self.value = None   #logical value of variable
+
+class Graph:
+    nodes = {}              #string variable marks it's vertex object
+    neighbours = {}         #dictionary of vertices with set of directed vertices
+                            #it can be used to construct graph
+    is_satisfiable = None   #boolean satisfiability indicator
+                            #it is None while it's value not known
+    
+    def __init__(self, expression):
+        #transform expression string
+        expression = expression.replace('(', ' ')
+        expression = expression.replace(')', ' ')
+        expression = expression.replace('+', ' ')
+        expression = expression.split()
+        #initialize nodes dictionary with unique nodes 
+        for v in set(expression):
+            self.nodes[v] = Vertex(v)
+        #initialize 'vertices : set of neighbours' dictionary
+        for n_key in self.nodes.keys():
+            self.neighbours[self.nodes[n_key]] = set()
