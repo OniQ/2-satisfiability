a20e4ed116fe87f2bc057ceaab58c804a719584d
OniQ <oniq.eg@gmail.com>
2014-03-14 18:38:35 +0200

    Add new example expression with different letters

--- src/test.py
+++ src/test.py
@@ -1,8 +1,9 @@
 from graph import Graph
 
-expression = "(u1 + u2)(u1 + ~u2)(u2 + u3)(~u1 + ~u3)"
+#expression = "(u1 + u2)(u1 + ~u2)(u2 + u3)(~u1 + ~u3)"
 #expression = "(u1 + u2)(~u1 + u3)(~u3 + ~u4)(u1 + u4)"
 #"(~u2 + ~u5)(u5 + ~u6)(u2 + u6)(~u3 + u4)"
+expression = "(~x + y)(~y + z)(x + ~z)(z + y)"
 graph = Graph(expression)
 graph.tarjan()
 graph.evaluate()
